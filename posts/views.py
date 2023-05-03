28295ddac005b18503402f0ae39059c9e40e314e
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2023-05-03 16:56:37 +0530

    all set

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+# posts/views.py
+from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Post
+from .serializers import PostSerializers
+# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
