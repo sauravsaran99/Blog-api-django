ca6ea052e7103adb60078e0ab37f5fb00a9a128e
Saurav Kumar <66282953+sauravsaran99@users.noreply.github.com>
2023-05-07 07:02:24 +0530

    Authentication implemented by using dj_rest_auth and django_allauth

--- django_project3/settings.py
+++ django_project3/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites", # new
     # 3rd-party apps
     "rest_framework", # new
     "corsheaders", # new
+    "rest_framework.authtoken", # new
+    "dj_rest_auth", # new
+    "allauth", # new
+    "allauth.account", # new
+    "allauth.socialaccount", # new
+    "dj_rest_auth.registration", # new
     # Local
     "accounts.apps.AccountsConfig", # new
     "posts.apps.PostsConfig", 
@@ -48,6 +55,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = { # new
 "DEFAULT_PERMISSION_CLASSES": [
 # "rest_framework.permissions.IsAuthenticated",
+],
+"DEFAULT_AUTHENTICATION_CLASSES": [ # new
+"rest_framework.authentication.SessionAuthentication",
+# "rest_framework.authentication.BasicAuthentication",
+"rest_framework.authentication.TokenAuthentication", #new
 ],
 }
 
@@ -83,10 +95,14 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.request", # new
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" # new
+SITE_ID = 1 # new
 
 WSGI_APPLICATION = 'django_project3.wsgi.application'
 
